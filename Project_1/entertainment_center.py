e67404b0201fc2c93013f48ddb64cbd03e8ecd71
Sainidhinkaminipaty <37266186+Sainidhinkaminipaty@users.noreply.github.com>
2020-09-26 10:43:02 +0200

    Update entertainment_center.py

--- Project_1/entertainment_center.py
+++ Project_1/entertainment_center.py
@@ -9,13 +9,13 @@ moonlight = media.Movie("MOON LIGHT",
                         "https://www.youtube.com/watch?v=9NJj12tJzqc")
 
 # Spider Man movie: Movie name, poster url and trailer youtube url
-spiderman = media.Movie("Spider Man",
+spiderman = media.Movie("SPIDER MAN",
                         "https://www.technobuffalo.com/wp-content/uploads" +
                         "/2017/07/SpiderManPoster.jpg",
                         "https://www.youtube.com/watch?v=DiTECkLZ8HM")
 
 # Mother movie: movie name, poster url and trailer youtube url
-Mother = media.Movie("The Mother",
+Mother = media.Movie("THE MOTHER",
                      "https://i0.wp.com/teaser-trailer.com/wp-content" +
                      "/uploads/Mother-Movie-New-Poster-2.jpg?ssl=1",
                      "https://www.youtube.com/watch?v=XpICoc65uh0")
